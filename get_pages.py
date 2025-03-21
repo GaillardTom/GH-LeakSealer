104163c0abaffba2b463c2a4ed4ebd49f94b151c
Tom Gaillard <LE1826622@crc-lennox.qc.ca>
2025-03-21 01:27:20 -0400

    Using official API now to search for the code still need to create issue with the off API but works like a charm now

--- get_pages.py
+++ get_pages.py
@@ -1,3 +1,4 @@
+import pymongo.errors
 import requests 
 from bs4 import BeautifulSoup
 
@@ -8,7 +9,7 @@ import json
 import re
 import time
 import pymongo
-from push_issue import push_issue_to_gh
+from push_issue import push_issue_to_gh, get_repos_id, CheckIfIssueExists, connect_to_mongodb
 
 
 def setup(): 
@@ -35,19 +36,37 @@ def getMongoURL(code):
     #use regex to get the mongo url from the code snippet which is a string
     #Starts with mongodb+srv:// and ends .net/ or .com/
     pattern = r"mongodb\+srv:\/\/[^\/\s]+"
+    # Search for all matches with the regex pattern and returns them all
     match = re.search(pattern, code)
-    return match.group(0) if match else None
+    # print(match.string)
+    if match: 
+        return match.group(0) if match.group(0) else None
+    else: 
+        return None
+
 def TestConnectionAndListDatabases(url):
     #Test the connection to the mongodb server and list all the databases
     #Use the pymongo library to connect to the mongodb server
     #Use the pymongo library to list all the databases
     #Return the list of databases
-    try:
+    if url == "" or url == None: 
+        return None
+    print(f"Trying to connect to {url}")
+    try: 
         client = pymongo.MongoClient(url+"/?retryWrites=true&w=majority")
         databases = client.list_database_names()
+        client.close()
         return databases
     except Exception as e:
-        return None
+        pass
+            
+def ResetDict(userDict):
+    #Reset the dictionary to None
+    userDict['mongo_url'] = None
+    userDict['repos_id'] = None
+    userDict['code'] = None
+    userDict['repos_url'] = []
+    return userDict
 def getResultForPage(page_number):
     global counter 
     
@@ -56,9 +75,9 @@ def getResultForPage(page_number):
     #Include all the characters in the search query to get all the results
     #Also include numbers in the search query 
     #Also include special characters in the search query
-    char = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '!']
+    char = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '!']
     for ch in char:
-        time.sleep(1)
+        time.sleep(0.8)
         url = f'https://github.com/search?q=mongodb%2Bsrv%3A%2F%2F{ch}+NOT+is%3Aarchived&type=code&ref=advsearch&p={page_number}'
         soup = get_pages(url)
         for tag in get_code_snippet(soup): 
@@ -68,25 +87,38 @@ def getResultForPage(page_number):
                     "owner": "",
                     "repos_url": "",
                     "code": "",
-                    "mongo_url": "",
+                    "mongo_url": [],
                 }  
                 code_dict['owner'] = owner.text.split('/')[0]
                 code_dict['repos_url'] = f"https://github.com/{owner['href']}"
                 code_dict['code'] = tag.text
                 code_dict['mongo_url'] = getMongoURL(tag.text)
+                code_dict['repos_id'] = get_repos_id(code_dict, session)
+                # print(f"Code snippet: {code_dict['code']}")
+                print(f"Mongo URLs: {code_dict['mongo_url']}")
+                print(f"Current owner: {owner}")
+                if CheckIfIssueExists(code_dict['repos_id'], connect_to_mongodb(os.getenv('mongo_url'))):
+                    print(f"Issue already exists in repos {code_dict.get('repos_url')}")
+                    code_dict = ResetDict(code_dict)
+                    break
                 if code_dict['mongo_url']:
                     db = TestConnectionAndListDatabases(code_dict['mongo_url'])
-                    if db:
+                    if db and not db == None:
                         counter += 1
                         print(f"Counter: {counter}")
                         print(f"Owner: {code_dict['owner']}, repos: {code_dict['repos_url']}, ,connection_string: {code_dict['mongo_url']}")
-                        print(f"Repos URL: {code_dict['repos_url']}")
+                        # print(f"Repos URL: {code_dict['repos_url']}")
                         for d in db: 
                             print(f"Database: {d}")
                         #Push the issue to github with the message
                         push_issue_to_gh(code_dict, db, session)
-                    res.append(code_dict)
+                        res.append(code_dict)
+                        code_dict = ResetDict(code_dict)
+                    else: 
+                        code_dict = ResetDict(code_dict)
+                        pass
                 else:
+                    code_dict = ResetDict(code_dict)
                     pass
     return res
     
--- push_issue.py
+++ push_issue.py
@@ -5,7 +5,6 @@ import json
 import os  
 from dotenv import load_dotenv
 import pymongo
-from bs4 import BeautifulSoup
 
 
 
@@ -17,14 +16,10 @@ from bs4 import BeautifulSoup
 load_dotenv()
 
 def get_repos_id(userDict, currentSession: requests.sessions): 
-    for k, v in userDict.items():
-        print(f"{k} == {v}")
     gh_api_headers = {"Authorization": "token " + os.getenv('gh_token')}
-    print(f"https://api.github.com/repos/{userDict['owner']}/{userDict['repos_url'].split("/")[-1]}")
     json_data = currentSession.get(f"https://api.github.com/repos/{userDict['owner']}/{userDict['repos_url'].split("/")[-1]}", headers=gh_api_headers, cookies=json.loads(os.getenv('burp0_cookies'))).json()
-    time.sleep(1)
+    # time.sleep(0.5)
     if json_data and json_data.get("node_id"): 
-        print(f"Found repos id for {userDict.get('repos_url')} == {json_data.get('node_id')}")
         return json_data.get("node_id")
     else: 
         raise Exception("Failed to get repos id")
@@ -44,7 +39,7 @@ def CheckIfIssueExists(repos_id, ownDBClient: pymongo.MongoClient):
         #Check if the result is not empty
         for res in result: 
             if res.get("repos_id") == str(repos_id):
-                print(f"Found issue in DB for repos {res.get('repos_id')} == {repos_id}")
+                # print(f"Found issue in DB for repos {res.get('repos_id')} == {repos_id}")
                 return True
         return False
     else: 
@@ -60,18 +55,21 @@ def addIssueToDB(repos_id, ownDBClient: pymongo.MongoClient):
     else:
         return Exception("No connection to MongoDB")
     
-def push_issue_to_gh(userDict, dabaseScanned: list[str], currentSession: requests.sessions): 
-    url = os.getenv('mongo_url')
-    db_client = connect_to_mongodb(url)
-    if(CheckIfIssueExists(userDict.get("repos_id"), db_client)):
-        print(f"Issue already exists in repos {userDict.get('repos_url')}")
-        return False
-    
+def push_issue_to_gh(userDict, dabaseScanned: list[str], currentSession: requests.sessions, db_client): 
     #Small func to convert the databases found to a string
+    def url_to_string():
+        if not userDict.get("mongo_url") or userDict.get("mongo_url") == []:
+            return None
+        url = userDict.get("mongo_url")
+        # Just to get the cluster without the username and password to raise awareness
+        url = url.split("@")[1]
+        return url
     def db_to_string(): 
         return "\r\n- ".join(dabaseScanned)    
-    repos_id = get_repos_id(userDict, currentSession)
-    json_data ={"query": "0c202ff06c8b13ded4c078a1440a8a58", "variables": {"fetchParent": False, "input": {"body": f"\n> [!WARNING]\n>  # You have an exposed mongoDB cluster containing multiple databases in this repository.\n#### Hey {userDict.get("owner")}, If you receive this issue don't panic, I am a friendly automated script looking around the internet and just to let you know that you have an **exposed mongoDB cluster in your code**.\n\n### I was able to connect and expose those databases from your cluster: \r\n- {db_to_string()}\n\n\n\nA **malicious attacker could leak data and get credentials** to your or people's services/system, even if you know that no sensible information is stored inside it, it is still very dangerous. I do not know what kind of information your databases hold but a malicious attacker could easily dump all the content, please **make sure to follow these steps**: \n\n1. Put your secrets in a .env file\n2. Use a library like [dotenv](https://www.npmjs.com/package/dotenv) to load the environment variables from your file onto your code\n3. At this point, I would either suggest either using [github's tool ](https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/removing-sensitive-data-from-a-repository) to erase the history or you could delete the repos on Github, remove the .git folder locally and recreate a new repos with a clean history\n\nIn the future make sure to **not expose your secrets** especially your mongodb uri as it contains your username and password combination. Make sure to **create a .env file and load your environment variables into your code accordingly**.\n\n### If you like what I am doing for the community, please feel free to follow my github account @GaillardTom \n\n\n", "issueTypeId": None, "parentIssueId": None, "repositoryId": repos_id, "title": "Important: Exposed MongoDB cluster in your code"}}}
+
+    print(f"Posting with theses databases \n- {db_to_string()} and these urls:  {url_to_string()}")
+    repos_id = userDict.get("repos_id")
+    json_data ={"query": "0c202ff06c8b13ded4c078a1440a8a58", "variables": {"fetchParent": False, "input": {"body": f"\n> [!WARNING]\n>  # You have an exposed mongoDB cluster containing multiple databases in this repository.\n#### Hey {userDict.get("owner")}, If you receive this issue don't panic, I am a friendly automated script looking around the internet and just to let you know that you have an **exposed mongoDB cluster in your code** that I got from this file {userDict.get("repos_file_url")}.\n\n### I was able to connect and expose those databases from your cluster: \r\n- {db_to_string()}\n\n\n\n **From these possible clusters: {url_to_string()}** \n\n\n\nA **malicious attacker could leak data and get credentials** to your or people's services/system, even if you know that no sensible information is stored inside it, it is still very dangerous. I do not know what kind of information your databases hold but a malicious attacker could easily dump all the content, please **make sure to follow these steps**: \n\n1. Put your secrets in a .env file\n2. Use a library like [dotenv](https://www.npmjs.com/package/dotenv) to load the environment variables from your file onto your code\n3. At this point, I would either suggest either using [github's tool ](https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/removing-sensitive-data-from-a-repository) to erase the history or you could delete the repos on Github, remove the .git folder locally and recreate a new repos with a clean history\n\nIn the future make sure to **not expose your secrets** especially your mongodb uri as it contains your username and password combination. Make sure to **create a .env file and load your environment variables into your code accordingly**.\n\n### If you like what I am doing for the community, please feel free to follow my github account @GaillardTom \n\n\n", "issueTypeId": None, "parentIssueId": None, "repositoryId": repos_id, "title": "Important: Exposed MongoDB cluster in your code"}}}
     currentSession.post("https://github.com:443/_graphql", headers=json.loads(os.getenv("burp0_headers_issue")), cookies=json.loads(os.getenv('burp0_cookies_issue')), json=json_data)
     res = addIssueToDB(repos_id, db_client)
     if res.inserted_id:
